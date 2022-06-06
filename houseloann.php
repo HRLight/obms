5906bfabaf58fbad11bc9e591fcf9b59d32538f0
HRLight <91833561+HRLight@users.noreply.github.com>
2022-06-06 03:57:59 -0400

    matapos ka sana

--- houseloann.php
+++ houseloann.php
@@ -1,39 +1,203 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
 <?php 
 
 include 'includes/head.php';
-
-
+$con1= mysqli_connect("localhost","root","","bank");
+$con2= mysqli_connect("localhost","root","","bank");
+$con3= mysqli_connect("localhost","root","","bank");
  ?>
 
-        <!-- Masthead-->
-        <header class="masthead">
-            <div class="container px-4 px-lg-5 h-100">
-                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-                    <div class="col-lg-8 align-self-end">
-                        <h1 class="text-white font-weight-bold">OBMS <br>Banking and Finance</h1>
-                        <hr class="divider" />
-                    </div>
-                    <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 mb-5">OBMS is a Banking and Finance Management System aims to create a system that is simple to use and improves bank transactions.</p>
-                        <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- About-->
 
-        <section class="page-section bg-primary" id="about">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-lg-8 text-center">
-                        <h2 class="text-white mt-0">About Us</h2>
-                        <hr class="divider divider-light" />
+ <?php
+        if(isset($_POST['add_vis'])){
+function generatekey(){
+  $keyLenght=8;
+  $str="1234567890";
+  $randStr=substr(str_shuffle($str),0,$keyLenght);
+  return $randStr;
+}
+$i=generatekey();
+  $house_id =$i;
+  $L_name = $_POST['l-name'];
+  $F_name = $_POST['f-name'];
+  $M_name = $_POST['m-name'];
+   $fullname=$F_name;
+    $fullname .= " ";
+  $fullname .=$M_name;
+  $fullname .= " ";
+  $fullname .=$L_name;
+  $L_amount = $_POST['l-amount'];
+  $L_term = $_POST['l-term'];
+  $sex = $_POST['gender'];
+  $civil_status = $_POST['civil-status'];
+  $perma_address = $_POST['per_address'];
+  $d_birth = $_POST['date'];
+  $p_birth = $_POST['birth_place'];
+  $m_number = $_POST['number'];  
+  $e_address = $_POST['email'];
+  $tin_sss_gsis_no = $_POST['tin-sss-gsis-no'];
+  $source_income = $_POST['source-income'];
+  $monthly_income = $_POST['m-income'];
+  $employeer_name = $_POST['e-name'];
+  $position = $_POST['position'];
+  $companyname = $_POST['companyname'];
+  $bankname = $_POST['bankname'];
+  $accountnum = $_POST['accountnum'];
+  $firstid = $_POST['firstid'];
+  $secondid = $_POST['secondid'];
+  $thirdid = $_POST['thirdid'];
+  
+  
+    
+      $sql  = "INSERT INTO `mortgages`( `ref_no`, `loan_amount`, `loan_terms`,`fname`, `mname`, `lname`, `sex`, `civil_status`,`perma_address`, `date_0f_birth`, `place_of_birth`, `mobile_no`, `email_address`, `tin_sss_gsis_no`, `source_of_income`, `monthly_income`, `employeer_name`,`position`,`companyname`,`bankname`,`accountnum`, `firstid`, `secondid`, `thirdid`) VALUES ('$house_id','$L_amount','$L_term','$F_name','$M_name','$L_name','$sex','$civil_status','$perma_address','$d_birth','$p_birth','$m_number','$e_address','$tin_sss_gsis_no','$source_income','$monthly_income','$employeer_name','$position','$companyname','$bankname','$accountnum','$firstid','$secondid','$thirdid')";
 
-                        <p class="text-white-75 mb-4"> The goal of this project is to fulfill a portion of the Bachelor of Science in Information Technology requirements.<br> The design and development of Online Banking Transaction Management provide a more secure way of managing bank customer information,<br> building strong relationships between banks and their customers by providing proper solutions that use multi-level confidentiality to<br> increase customer satisfaction and provide an easy way of communicating with the bank and the client.</p>
-                        <a class="btn btn-light btn-xl" href="#services">Get Started!</a>
-                    </div>
-                </div>
-            </div>
+      $query="INSERT INTO `loan_processing_approval`(`id`, `accountnum`,`Full_Name`, `Ref_Number`, `Type_of_loan`, `Amount_loan`, `Status`) VALUES ('','$accountnum','$fullname ','$house_id','House Loan','$L_amount','Pending')";
+
+if ($con1->query($sql)===TRUE AND $con2->query($query)===TRUE) {
+                echo '<script type="text/javascript">
+                    swal("", "Succesfully Encoded", "success").then(function() {
+                    window.location = "houseloann.php";});
+                  </script>';
+
+            
+        }
+    }
+
+      ?>
+
+        <section class="page-section bg-primary" style="padding:100px;" id="about">
+           
+  <!--Appplication Form -->
+    <div class="applcation_form">
+
+         <form method="POST">
+
+         <table class="table table-bordeless">
+         </table>
+
+        <table class="table table-bordeless">
+          <thead class="thead-dark">
+            <!-- Start OF Application Form-->
+          <tr>
+            <th colspan="5"> Housing Application Form</th>
+          </tr>
+         </thead>
+          <tbody>
+            <tr>
+              <td colspan="1"><label>Last Name:</label> <input type="text" name="l-name" id="formControlLg" class="form-control form-control-lg" placeholder="Last Name" required/> </td>
+              <td colspan="2"><label>Frist Name:</label>  <input type="text" name="f-name" id="formControlLg" class="form-control form-control-lg" placeholder="First Name" required /></td>
+              <td><label>Middle Name:</label><input type="text"  name="m-name" id="formControlLg" class="form-control form-control-lg" placeholder="Middle Name" required /></td>
+            </tr>
+
+              <tr>
+              <td><label>Loan Amount:</label><input type="number"  name="l-amount" id="formControlLg" class="form-control form-control-lg" placeholder="Loan Amount" required /></td>
+
+              <td><label>Loan Term</label>
+                <select   name="l-term" class="form-control  form-control-lg">
+                          <option>Select</option>
+                          <option value="Long Term">Long Term (1-25 Years)</option>
+                          <option value="Short Term">Short Term(3-11 Months)</option>
+                          <option value="Intermediate Term">Intermediate Term(1-15 Years)</option>
+                        </select> 
+              </td>
+
+              <td><label>Gender</label>
+                <select   name="gender" class="form-control  form-control-lg">
+                          <option>Select</option>
+                          <option value="male">Male</option>
+                          <option value="female">Female</option>
+                        </select>
+              </td>
+
+              </tr> 
+              <tr>
+              <td><label>Civil Status:</label>
+                  <select name="civil-status" class="form-control  form-control-lg">
+                          <option>Select</option>
+                          <option value="single">Single</option>
+                          <option value="married">Married</option>
+                          <option value="divorsed">Divorsed</option>
+                          <option value="common_law">Common-Law</option>
+                        </select>
+              </td>
+
+
+              <td colspan="2"><label> Permanent address :</label>
+              <input type="text" id="formControlLg" name="per_address" class="form-control form-control-lg" placeholder="Full Address" required />
+              </td>
+
+              <td> <label>Birth Date:</label>
+
+                <input type="date" name="date" class="form-control form-control-lg" />
+                   </td>
+
+              <td><label>Birth Place:</label>
+                <input type="text" id="formControlLg" name="birth_place" class="form-control form-control-lg" placeholder="Birth Place" required /></td>
+            </tr>
+
+            <tr>
+            <td colspan="2"><label>Mobile no/s:
+            </label><input type="number" name="number" id="formControlLg" class="form-control form-control-lg" placeholder="+63" required /></td>
+
+            <td colspan="2"><label>Emails Address:</label><input type="email" name="email" id="formControlLg" class="form-control form-control-lg" placeholder="email@gmail.com" required /></td>
+
+            <td colspan="2"><label>Tin/Sss/Gsis no#:
+            </label><input type="number" name="tin-sss-gsis-no" id="formControlLg" class="form-control form-control-lg" placeholder="Tin/Sss/Gsis no#" required /></td>
+            </tr>
+
+              <td><label>Source of Income:</label>
+                  <select name="source-income" class="form-control  form-control-lg">
+                          <option>Select</option>
+                          <option value="Business">Business</option>
+                          <option value="Employment">Employment</option>
+                        </select>
+              </td>
+
+              <td><label>Monthly Income:</label> <input type="text" name="m-income" id="formControlLg" class="form-control form-control-lg" placeholder="Monthly Income" required /></td>
+
+              <td><label>Employer Name:</label> <input type="text" name="e-name" id="formControlLg" class="form-control form-control-lg" placeholder="Employer Name" required /></td>
+
+              <td><label>Position:</label> <input type="text" name="position" id="formControlLg" class="form-control form-control-lg" placeholder="Position in Work" required /></td>
+            </tr>
+
+            <td colspan="2"><label>Company Name:</label>  <input type="text" name="companyname" id="formControlLg" class="form-control form-control-lg" placeholder="Company Name" required /></td>
+
+              <td colspan="2"><label>Bank Name:</label>  <input type="text" name="bankname" id="formControlLg" class="form-control form-control-lg" placeholder="Bank Name" required /></td>
+
+              <td><label>Account Number:</label><input type="number"  name="accountnum" id="formControlLg" class="form-control form-control-lg" placeholder="Account Number" required /></td>
+            </tr>
+
+                <td>
+                 <label style="color: red ;"> *If employeed upload your copy of pay slip for verification. </label> <br>
+                </td>
+              <td colspan="2"><span>Please upload your ID picture</span> <input type="file" name="firstid" id="formControlLg" class="form-control form-control-lg" /></td>
+              </tr>
+              <tr>
+                <td>
+                 <label style="color: red ;"> *Please upload your ID here. </label> <br>
+                </td>
+              <td colspan="2"><span>Please upload your ID picture</span> <input type="file" name="secondid" id="formControlLg" class="form-control form-control-lg" required /></td>
+              </tr>
+              <tr>
+                <td>
+                 <label style="color: red ;"> *Please upload your ID here. </label> <br>
+                </td>
+              <td colspan="2"><span>Please upload your ID picture</span> <input type="file" name="thirdid" id="formControlLg" class="form-control form-control-lg" required /></td>
+            </tr>
+
+
+         </thead>
+          </tbody>          
+        </table>
+           <button type="submit" class="submit" name="add_vis">Apply Now!</button>
+          </form>
+          <br><br><br><br>
+
+
+
+
+
         </section>
 
 
--- personalloann.php
+++ personalloann.php
@@ -1,40 +1,238 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
 <?php 
 
 include 'includes/head.php';
-
-
-
+$con1= mysqli_connect("localhost","root","","bank");
+$con2= mysqli_connect("localhost","root","","bank");
+$con3= mysqli_connect("localhost","root","","bank");
  ?>
 
-        <!-- Masthead-->
-        <header class="masthead">
-            <div class="container px-4 px-lg-5 h-100">
-                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-                    <div class="col-lg-8 align-self-end">
-                        <h1 class="text-white font-weight-bold">OBMS <br>Banking and Finance</h1>
-                        <hr class="divider" />
-                    </div>
-                    <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 mb-5">OBMS is a Banking and Finance Management System aims to create a system that is simple to use and improves bank transactions.</p>
-                        <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- About-->
+  <?php  
 
-        <section class="page-section bg-primary" id="about">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-lg-8 text-center">
-                        <h2 class="text-white mt-0">About Us</h2>
-                        <hr class="divider divider-light" />
+  if(isset($_POST['add_vis'])){
+  function generatekey(){
+  $keyLenght=8;
+  $str="1234567890";
+  $randStr=substr(str_shuffle($str),0,$keyLenght);
+  return $randStr;
+}
+$i=generatekey();
+  $house_id =$i;
+  $first_name = $_POST['f-name'];
+  $middle_name = $_POST['m-name'];
+  $last_name = $_POST['l-name'];
+  $fullname=$first_name;
+  $fullname .= " ";
+  $fullname .=$middle_name;
+  $fullname .=" ";
+  $fullname .=$last_name;
+  $birthdate = $_POST['date'];
+  $birth_place = $_POST['birth_place'];
+  $gender = $_POST['gender'];
+  $phone_no = $_POST['number'];  
+  $email = $_POST['email'];
+  $type_of_id = $_POST['t-id'];
+  $id_no = $_POST['id-no'];
+  $perma_address = $_POST['per_address'];
+  $civil_status = $_POST['civil-status'];
+  $place_of_work = $_POST['p-work'];
+  $job_title = $_POST['j-title'];
+  $years_of_employeed = $_POST['y-employ'];
+  $monthly_income = $_POST['m-income'];
+  $lamount = $_POST['lamount'];
+  $purpose_of_loan = $_POST['p-loan'];
+  $gender = $_POST['gender'];
+  $loan_term = $_POST['l-term'];
+  $companyname = $_POST['companyname'];
+  $bankname = $_POST['bankname'];
+  $accountnum = $_POST['accountnum'];
+  $guarantor_name = $_POST['g-name'];
+  $relation = $_POST['relation'];
+  $guarantor_place_of_work = $_POST['g-p-w'];
+  $guarantor_income = $_POST['g-income'];
+  $guarantor_id_type = $_POST['g-idtype'];
+  $guarantor_id_number = $_POST['g-idnumber'];
+  $guarantor_phone_number = $_POST['g-pnumber'];
+  $fid = $_POST['fid'];
+  $sid = $_POST['sid'];
+  $tid = $_POST['tid'];
 
-                        <p class="text-white-75 mb-4"> The goal of this project is to fulfill a portion of the Bachelor of Science in Information Technology requirements.<br> The design and development of Online Banking Transaction Management provide a more secure way of managing bank customer information,<br> building strong relationships between banks and their customers by providing proper solutions that use multi-level confidentiality to<br> increase customer satisfaction and provide an easy way of communicating with the bank and the client.</p>
-                        <a class="btn btn-light btn-xl" href="#services">Get Started!</a>
-                    </div>
-                </div>
-            </div>
+
+   $sql  = "INSERT INTO `personal_loan`(`id`, `first_name`, `middle_name`, `last_name`, `birthdate`, `birth_place`,`gender`, `phone_no`, `email`, `type_of_id`, `id_no`, `perma_address`, `civil_status`, `place_of_work`, `job_title`, `years_of_employeed`, `monthly_income`,`lamount`, `purpose_of_loan`, `loan_term`,`companyname`,`bankname`,`accountnum`, `guarantor_name`, `relation`, `guarantor_place_of_work`, `guarantor_income`, `guarantor_id_type`, `guarantor_id_number`, `guarantor_phone_number`, `fid`, `sid`, `tid`) VALUES ('$house_id','  $first_name','$middle_name','$last_name','$birthdate','$birth_place','$gender','$phone_no','$email','$type_of_id','$id_no','$perma_address','$civil_status','$place_of_work','$job_title','$years_of_employeed','$monthly_income','$lamount','$purpose_of_loan','$loan_term','$companyname','$bankname','$accountnum','$guarantor_name','$relation','$guarantor_place_of_work','$guarantor_income','$guarantor_id_type','$guarantor_id_number','$guarantor_phone_number','$fid','$sid','$tid')";
+      
+         $query="INSERT INTO `loan_processing_approval`(`id`, `accountnum`,`Full_Name`, `Ref_Number`, `Type_of_loan`, `Amount_loan`, `Status`) VALUES ('','$accountnum','$fullname ','$house_id','Personal Loan','$lamount','Pending')";
+if ($con1->query($sql)===TRUE AND $con2->query($query)===TRUE) {
+                echo '<script type="text/javascript">
+                    swal("", "Succesfully Encoded", "success").then(function() {
+                    window.location = "personalloann.php";});
+                  </script>';
+                }
+}
+
+        ?>
+
+
+        <section class="page-section bg-primary" style="padding: 100px" id="about">
+                
+                <!--Appplication Form -->
+    <div class="applcation_form">
+
+         <form method="POST">
+
+         <table class="table table-bordeless">
+         </table>
+
+        <table class="table table-bordeless">
+          <thead class="thead-dark">
+            <!-- Start OF Application Form-->
+          <tr>
+            <th colspan="5"> Personal Application Form</th>
+          </tr>
+         </thead>
+          <tbody>
+            <tr>
+              <td colspan="2"><label>First Name:</label>  <input type="text" name="f-name" id="formControlLg" class="form-control form-control-lg" placeholder="First Name" required /></td>
+
+              <td colspan="2"><label>Middle Name:</label><input type="text"  name="m-name" id="formControlLg" class="form-control form-control-lg" placeholder="Middle Name" required /></td>
+
+              <td colspan="2"><label>Last Name:</label> <input type="text" name="l-name" id="formControlLg" class="form-control form-control-lg" placeholder="Last Name" required/> </td>
+            </tr>
+
+            <tr>
+            <td> <label>Birth Date:</label>
+
+                <input type="date" name="date" class="form-control form-control-lg" />
+                   </td>
+
+              <td><label>Birth Place:</label>
+                <input type="text" id="formControlLg" name="birth_place" class="form-control form-control-lg" placeholder="Birth Place" required /></td>
+
+               <td><label>Gender</label>
+                <select   name="gender" class="form-control  form-control-lg">
+                          <option>Select</option>
+                          <option value="male">Male</option>
+                          <option value="female">Female</option>
+                        </select>
+              </td>
+              
+            <td colspan="2"><label>Mobile no/s:
+            </label><input type="number" name="number" id="formControlLg" class="form-control form-control-lg" placeholder="+63" required /></td>
+
+            <td colspan="2"><label>Emails Address:</label><input type="email" name="email" id="formControlLg" class="form-control form-control-lg" placeholder="email@gmail.com" required /></td>
+          </tr>
+
+            <tr>
+            <td colspan="2"><label>Type of ID:
+            </label><input type="text" name="t-id" id="formControlLg" class="form-control form-control-lg" placeholder="Type of ID" required /></td>
+        
+            <td colspan="2"><label>ID Number:
+            </label><input type="number" name="id-no" id="formControlLg" class="form-control form-control-lg" placeholder="ID Number" required /></td>
+
+            <td colspan="2"><label> Permanent address :</label>
+              <input type="text" id="formControlLg" name="per_address" class="form-control form-control-lg" placeholder="Full Address" required />
+              </td>
+            </tr>
+            <tr>
+              <td><label>Civil Status:</label>
+                  <select name="civil-status" class="form-control  form-control-lg">
+                          <option>Select</option>
+                          <option value="single">Single</option>
+                          <option value="married">Married</option>
+                          <option value="divorsed">Divorsed</option>
+                          <option value="common_law">Common-Law</option>
+                        </select>
+              </td>
+
+              <td colspan="2"><label> Place of Work :</label>
+              <input type="text" id="formControlLg" name="p-work" class="form-control form-control-lg" placeholder="Place of Work" required />
+              </td>
+
+              <td><label>Job Title:</label> <input type="text" name="j-title" id="formControlLg" class="form-control form-control-lg" placeholder="Job Title" required /></td>
+
+              <td colspan="2" ><label>Years of Employed:</label> <input type="text" name="y-employ" id="formControlLg" class="form-control form-control-lg" placeholder="Years of Employed" required /></td>
+              </tr>
+              <tr>
+              <td><label>Monthy Income:</label> <input type="number" name="m-income" id="formControlLg" class="form-control form-control-lg" placeholder="Monthly Income" required /></td>
+
+              <td><label>Loan Amount:</label> <input type="number" name="lamount" id="formControlLg" class="form-control form-control-lg" placeholder="Loan Amount" required /></td>
+
+             <td colspan="2" ><label>Purpose of Loan:</label>
+                  <select name="p-loan" class="form-control  form-control-lg">
+                          <option>Select</option>
+                          <option value="Medical Procedures">Medical Procedures</option>
+                          <option value="Vacation or Weddings">Vacation or Weddings</option>
+                          <option value="Home Improvesments">Home Improvesments</option>
+                        </select>
+              </td>
+
+              <td colspan="2" ><label>Loan Term</label>
+                <select   name="l-term" class="form-control  form-control-lg">
+                          <option>Select</option>
+                          <option value="Long Term">Long Term (1-25 Years)</option>
+                          <option value="Short Term">Short Term(3-11 Months)</option>
+                          <option value="Intermediate Term">Intermediate Term(1-15 Years)</option>
+                        </select> 
+              </td>
+            </tr>
+
+            <tr>
+                <td>
+                 <label style="color: red ;"> *For Verification please provide your bank details. </label> <br>
+                </td>
+                </tr>
+              <td colspan="2"><label>Company Name:</label>  <input type="text" name="companyname" id="formControlLg" class="form-control form-control-lg" placeholder="Company Name" required /></td>
+
+              <td colspan="2"><label>Bank Name:</label>  <input type="text" name="bankname" id="formControlLg" class="form-control form-control-lg" placeholder="Bank Name" required /></td>
+
+              <td><label>Account Number:</label><input type="number"  name="accountnum" id="formControlLg" class="form-control form-control-lg" placeholder="Account Number" required /></td>
+            </tr>
+
+              <td><label>Guarantor Name :</label> <input type="text" name="g-name" id="formControlLg" class="form-control form-control-lg" placeholder="Guarantor Name" required /></td>
+
+              <td colspan="2"><label>Relation to the Guarantor :</label> <input type="text" name="relation" id="formControlLg" class="form-control form-control-lg" placeholder="Relation to the Guarantor" required /></td>
+
+              <td colspan="2"><label>Guarantor Place of Work :</label> <input type="text" name="g-p-w" id="formControlLg" class="form-control form-control-lg" placeholder="Guarantor Place of Work" required /></td>
+
+              </tr>
+              <tr>
+              <td colspan="2"><label>Guarantor Monthly Income :</label> <input type="number" name="g-income" id="formControlLg" class="form-control form-control-lg" placeholder="Guarantor Monthly Income" required /></td>
+              
+              <td colspan="2" ><label>Guarantor ID Type :</label> <input type="text" name="g-idtype" id="formControlLg" class="form-control form-control-lg" placeholder="Guarantor ID Type" required /></td>
+              </tr>
+              <tr>
+              <td colspan="2"><label>Guarantor ID Number :</label> <input type="number" name="g-idnumber" id="formControlLg" class="form-control form-control-lg" placeholder="Guarantor ID Number" required /></td>
+
+              <td colspan="2"><label>Guarantor Phone Number :</label> <input type="number" name="g-pnumber" id="formControlLg" class="form-control form-control-lg" placeholder="Guarantor Phone Number" required /></td>
+            </tr>
+
+                <td>
+                 <label style="color: red ;"> *If employeed upload your copy of pay slip for verification. </label> <br>
+                </td>
+              <td colspan="2"><span>Please upload your ID picture</span> <input type="file" name="fid" id="formControlLg" class="form-control form-control-lg" /></td>
+              </tr>
+              <tr>
+                <td>
+                 <label style="color: red ;"> *Please upload your ID here. </label> <br>
+                </td>
+              <td colspan="2"><span>Please upload your ID picture</span> <input type="file" name="sid" id="formControlLg" class="form-control form-control-lg" required /></td>
+              </tr>
+              <tr>
+                <td>
+                 <label style="color: red ;"> *Please upload your ID here. </label> <br>
+                </td>
+              <td colspan="2"><span>Please upload your ID picture</span> <input type="file" name="tid" id="formControlLg" class="form-control form-control-lg" required /></td>
+            </tr>
+
+         </thead>
+          </tbody>          
+        </table>
+           <button type="submit" class="submit" name="add_vis">Apply Now!</button>
+          </form>
+          <br><br><br><br>
+
+
+
         </section>
 
 
